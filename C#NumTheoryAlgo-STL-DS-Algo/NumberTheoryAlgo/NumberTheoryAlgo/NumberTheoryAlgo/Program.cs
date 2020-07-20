346957310dc072a43f691dc088762549e38d6b9d
shohag_shadaf <shohag.mbstu.it16030@gmail.com>
2020-07-20 20:06:47 +0600

    C#practiceNumberTheroyAlgo-STL-DS-Algo

---
+++ C#NumTheoryAlgo-STL-DS-Algo/NumberTheoryAlgo/NumberTheoryAlgo/NumberTheoryAlgo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumberTheoryAlgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
